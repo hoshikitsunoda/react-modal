14c1a52d52e6205d8828d62e61b4f08007146096
Hoshiki Tsunoda <liquidohr@gmail.com>
2017-11-15 14:36:27 -0800

    Style it using styled-component and styles.css. #1

--- client/public/index.js
+++ client/public/index.js
@@ -3,18 +3,34 @@ import { render } from 'react-dom'
 import styled from 'styled-components'
 
 const Button = styled.button`
-  width: 100px;
-  height: 50px;
-  background-color: #F5F6F5;
+  width: 180px;
+  height: 80px;
+  background-color: #1774D7;
+  color: white;
+  font-size: 2rem;
 `
 
 const Pop = styled.div`
+  position: absolute;
   width: 500px;
-  height: 300px;
-  background-color: #282C33;
-  color: white;
-  text-align: center;
+  height: 205px;
+  background-color: #FEFFFE;
+  color: black;
   font-size: 50px;
+  left: calc(50% - 250px);
+  top: calc(50% - 102.5px);
+  border-radius: 5px;
+`
+
+const Shadow = styled.div`
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  top: 0;
+  left: 0;
+  background-color: rgba(0, 0, 0, 0.3);
+  z-index: -1;
+  animation: fadein 0.2s;
 `
 
 const show = { display: 'block' }
@@ -39,7 +55,14 @@ class Modal extends React.Component {
         <Button onClick={this.toggle}>
           {this.state.isToggleOn ? 'Launch Modal!' : 'Hide Modal!'}
         </Button>
-        <Pop className="modal" style={this.state.isToggleOn ? hide : show}>Modal!</Pop>
+        <Pop className="modal" style={this.state.isToggleOn ? hide : show}>
+          <h5>Modal title</h5>
+          <h6>Woohoo, you are reading this text in a modal!</h6>
+          <i className="fa fa-times" aria-hidden="true" onClick={this.toggle}></i>
+          <button className="close" onClick={this.toggle}>Close</button>
+          <button className="save">Save changes</button>
+        </Pop>
+        <Shadow style={this.state.isToggleOn ? hide : show}></Shadow>
       </div>
     )
   }
